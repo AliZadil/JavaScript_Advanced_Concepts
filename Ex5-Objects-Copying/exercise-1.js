5ed1681763254ed20ec6b737dae87d892d01eee3
Ali Zadil <tarikz94@gmail.com>
2023-04-03 23:51:44 +0300

    Ex5 completed

--- Ex5-Objects-Copying/exercise-1.js
+++ Ex5-Objects-Copying/exercise-1.js
@@ -4,9 +4,16 @@ const person1 = {
   age: 25,
 };
 
-const person2 = person1;
+const person2 = {...person1};
+person2.firstName = "Simon";
 
 // Modify the property `firstName` of the `person2` in "Simon".
+
+/* 
+if person2 is changed, when const person2 = person1,
+person1.firstName would become "Simon" as well. 
+It is because objects are reference types.
+*/
 
 console.log(person1);
 console.log(person2);
