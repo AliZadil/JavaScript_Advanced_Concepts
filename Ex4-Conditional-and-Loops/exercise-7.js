f6e2afd2dc233bc47b56523748df3f93624498c5
Ali Zadil <tarikz94@gmail.com>
2023-04-03 23:33:03 +0300

    Ex4 completed

--- Ex4-Conditional-and-Loops/exercise-7.js
+++ Ex4-Conditional-and-Loops/exercise-7.js
@@ -1,5 +1,16 @@
 function sumUntil(maxValue) {
-  return (maxValue*(maxValue+1)/2)
+  let result = 0;
+  for (let index = 0; index < maxValue; index++) {
+    result = result + index +1;
+  }
+  return(result);
 }
+
+/* 
+function sumUntil(maxValue) {
+  return (maxValue*(maxValue+1)/2);
+}
+this would be simpler, just saying...
+*/
 
 console.log(sumUntil(5));
