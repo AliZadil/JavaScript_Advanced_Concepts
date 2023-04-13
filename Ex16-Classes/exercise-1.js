2985eca32eaf44d4d7733f4827c4c0f763f33457
Ali Zadil <tarikz94@gmail.com>
2023-04-13 17:38:33 +0300

    updated

--- Ex16-Classes/exercise-1.js
+++ Ex16-Classes/exercise-1.js
@@ -1,6 +1,8 @@
-function Person(firstName,lastName){
+class Person{
+    constructor(firstName,lastName){
     this.firstName=firstName;
     this.lastName=lastName;
+    }
 }
 
 const developer = new Person('Mario', 'Rossi');
