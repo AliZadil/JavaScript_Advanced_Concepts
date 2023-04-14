e2ff6a0d2099bfbceb5ce2d2a93d428ed0997d96
Ali Zadil <tarikz94@gmail.com>
2023-04-14 17:11:38 +0300

    not done

--- Ex32-JSON-Methods/exercise-4.js
+++ Ex32-JSON-Methods/exercise-4.js
@@ -8,6 +8,12 @@ const person = {
   // } 
   // I couldnot find a way to use toJson in an object.
 };
+const filteredPerson ={
+  id: person.id,
+  age: person.age
+}
 
-const json = JSON.stringify(person);
+
+// const json = JSON.stringify(person);
+const json = JSON.stringify(filteredPerson);
 console.log(json); // Should return: { id: 1, age: 25 }
