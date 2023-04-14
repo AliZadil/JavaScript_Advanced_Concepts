a2316856a3f252902e01ba73e268b217545a8bb7
Ali Zadil <tarikz94@gmail.com>
2023-04-14 20:08:12 +0300

    updated

--- Ex19-Getters-and-Setters/exercise-7.js
+++ Ex19-Getters-and-Setters/exercise-7.js
@@ -1,46 +1,83 @@
+// Worked on it with Andrea //
+// class Person {
+//   firstName;
+//   lastName;
+//   age;
+//   constructor (firstName, lastName, age){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//   }
+//   set firstName(newFirst){
+//     if(newFirst !== String(newFirst)){
+//       this.firstName = String(newFirst);
+//     }else{
+//       this.firstName = newFirst;
+//     }
+//   }
+//   set lastName(newLast){
+//     if(newLast !== String(newLast)){
+//       this.lastName = String(newLast);
+//     }else{
+//       this.lastName = newLast;
+//     }
+//   }
+//   set age(newAge){
+//     if(newAge < 1){
+//       this.age = 1;
+//     }else if (newAge > 110){
+//       this.age = 110;
+//     }else {
+//       this.age = newAge;
+//     }
+//   }
+//   get firstName(){
+//     return this.firstName;
+//   }
+//   get lastName(){
+//     return this.lastName;
+//   }
+//   get age(){
+//     return this.age;
+//   }
+//   get fullName(){
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+
 class Person {
-  firstName;
-  lastName;
-  age;
-  constructor (firstName, lastName, age){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
+  constructor(firstName, lastName, age) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._age = age;
   }
-  set firstName(newFirst){
-    if(newFirst !== String(newFirst)){
-      this.firstName = String(newFirst);
-    }else{
-      this.firstName = newFirst;
-    }
+
+  get firstName() {
+    return this._firstName;
   }
-  set lastName(newLast){
-    if(newLast !== String(newLast)){
-      this.lastName = String(newLast);
-    }else{
-      this.lastName = newLast;
-    }
+
+  set firstName(value) {
+    this._firstName = value;
   }
-  set age(newAge){
-    if(newAge < 1){
-      this.age = 1;
-    }else if (newAge > 110){
-      this.age = 110;
-    }else {
-      this.age = newAge;
-    }
+
+  get lastName() {
+    return this._lastName;
   }
-  get firstName(){
-    return this.firstName;
+
+  set lastName(value) {
+    this._lastName = value;
   }
-  get lastName(){
-    return this.lastName;
+
+  get age() {
+    return this._age;
   }
-  get age(){
-    return this.age;
+
+  set age(value) {
+    this._age = value;
   }
-  get fullName(){
-    return `${this.firstName} ${this.lastName}`;
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
   }
 }
 
